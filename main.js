3b703cc1fef7d109b18ccf7e8de3555995045e63
efendly <ayselefendiyeva@icloud.com>
2023-05-10 15:37:25 +0400

    modul3

---
+++ main.js
@@ -0,0 +1,8 @@
+const url = 'https://api.exchanherate.host/latest';
+
+let currentCurr = 'RUB',exchangeCurr = 'USD'
+let currVal = 1;
+
+const inpleft = document.querySelector('.left-input');
+const  inpRight = document.querySelector('.right-input');
+
